0cf147c9640ef6c7c1fbb68c4a7ed81c780c637c
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-01-27 23:18:03 -0800

    UI design for suite nav bar

--- plugins/theme-rushstack-suite-nav/src/theme/Root/index.tsx
+++ plugins/theme-rushstack-suite-nav/src/theme/Root/index.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 // Default implementation, that you can customize
 function Root(props: { children: unknown }) {
-  const style: React.CSSProperties = {
-    height: '48px',
-    background: '#555555',
-    color: '#ffffff',
-    fontWeight: '500',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
-  };
-  const style2: React.CSSProperties = {
-    paddingLeft: '32px'
-  };
-  const style3: React.CSSProperties = {
-    width: '2px',
-    height: '80%',
-    background: '#808080'
-  };
-
   return (
     <>
-      <div style={style}>
-        <img
-          src={'/images/rs-hamburger.svg'}
-          style={{ height: '20px', paddingLeft: '16px', paddingRight: '16px' }}
-        />
-        <div style={style3} />
+      <div className={styles.suiteNavBar}>
+        <div role="button" className={styles.hamburger}>
+          <img src={'/images/rs-hamburger.svg'} />
+        </div>
+        <div className={styles.hamburgerDivider} />
 
-        <div style={style2}>Roadmap</div>
-        <div style={style2}> Shop</div>
-        <div style={style2}>Events</div>
-        <div style={style2}>Community</div>
+        <a href="https://rushstack.io/pages/overview/roadmap/" className={styles.suitNavItem}>
+          Roadmap
+        </a>
+        <a href="https://rushstack.io/pages/shop/" className={styles.suitNavItem}>
+          Shop
+        </a>
+        <a href="https://rushstack.io/community/events/" className={styles.suitNavItem}>
+          Events
+        </a>
       </div>
 
       {props.children}
